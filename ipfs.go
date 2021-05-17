b1012ecbc2984a8bdb19e2f0fb5fd0ad82dbdc8d
Pasi Hyvönen <pasi@koud.fi>
2021-05-17 18:30:09 +0300

    Fully implement basic functionality

--- ipfs.go
+++ ipfs.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"io"
 	"io/fs"
 	"os"
+	pathpkg "path"
+	"strings"
 	"sync"
+	"time"
 
 	config "github.com/ipfs/go-ipfs-config"
 	files "github.com/ipfs/go-ipfs-files"
@@ -55,80 +59,6 @@ func New(ctx context.Context, repoRoot string) (*FS, error) {
 	return &FS{n: n}, nil
 }
 
-func (f *FS) Open(name string) (fs.File, error) {
-	p := path.FromString(name)
-
-	// TODO: IPNS resolution
-
-	c, _, err := f.n.Resolver.ResolveToLastNode(f.n.Context(), p)
-	if err != nil {
-		return nil, fmt.Errorf("path resolve: %v", err)
-	}
-	fn, err := f.n.DAG.Get(f.n.Context(), c)
-	if err != nil {
-		return nil, fmt.Errorf("dag get: %v", err)
-	}
-	n, err := unixfile.NewUnixfsFile(f.n.Context(), f.n.DAG, fn)
-	if err != nil {
-		return nil, fmt.Errorf("create unix file: %v", err)
-	}
-	switch n := n.(type) {
-	case files.File:
-		return &File{File: n, p: p}, nil
-	case files.Directory:
-		return &Dir{Directory: n, p: p}, nil
-	default:
-		return nil, errors.New("invalid file type")
-	}
-}
-
-type File struct {
-	files.File
-	p path.Path
-}
-
-func (f *File) Stat() (fs.FileInfo, error) {
-
-	// ???
-
-	panic("TODO")
-}
-
-type Dir struct {
-	files.Directory
-	p path.Path
-}
-
-func (f *Dir) Read([]byte) (int, error) { return 0, fs.ErrInvalid }
-
-func (f *Dir) Stat() (fs.FileInfo, error) {
-
-	// ???
-
-	panic("TODO")
-}
-
-func (t *Dir) ReadDir(n int) ([]fs.DirEntry, error) {
-	/*
-		e := f.Entries()
-		for e.Next() {
-			s, _ := e.Node().Size()
-			fmt.Println(e.Name(), s)
-		}
-		if err := e.Err(); err != nil {
-			log.Fatal(err)
-		}
-	*/
-	panic("TODO")
-}
-
-type fileInfo struct {
-	name string
-
-	// TODO
-
-}
-
 func loadPlugins() error {
 	plugins, err := loader.NewPluginLoader("")
 	if err != nil {
@@ -149,3 +79,112 @@ func newDefaultConfig() (*config.Config, error) {
 	}
 	return config.InitWithIdentity(identity)
 }
+
+func (f *FS) Open(name string) (fs.File, error) {
+	p := path.FromString(name)
+
+	// TODO: IPNS resolution
+
+	c, _, err := f.n.Resolver.ResolveToLastNode(f.n.Context(), p)
+	if err != nil {
+		return nil, fmt.Errorf("path resolve: %v", err)
+	}
+	fn, err := f.n.DAG.Get(f.n.Context(), c)
+	if err != nil {
+		return nil, fmt.Errorf("dag get: %v", err)
+	}
+	n, err := unixfile.NewUnixfsFile(f.n.Context(), f.n.DAG, fn)
+	if err != nil {
+		return nil, fmt.Errorf("create unix file: %v", err)
+	}
+	switch n := n.(type) {
+	case files.File:
+		return &file{File: n, fileNode: fileNode{
+			p: p,
+			m: fs.ModePerm,
+		}}, nil
+	case files.Directory:
+		return &dir{Directory: n, fileNode: fileNode{
+			p: p,
+			m: fs.ModePerm | fs.ModeDir,
+		}}, nil
+	default:
+		return nil, errors.New("invalid file type")
+	}
+}
+
+type file struct {
+	files.File
+	fileNode
+}
+
+type dir struct {
+	files.Directory
+	fileNode
+}
+
+func (d *dir) Read([]byte) (int, error) { return 0, fs.ErrInvalid }
+
+func (d *dir) ReadDir(n int) ([]fs.DirEntry, error) {
+	var (
+		dir []fs.DirEntry
+		e   = d.Entries()
+	)
+	for e.Next() {
+		var m fs.FileMode
+		if strings.HasSuffix(e.Name(), "/") {
+			m = fs.ModeDir
+		}
+		dir = append(dir, dirEntry{
+			fileNode{Node: e.Node(), p: path.Path(e.Name()), m: m},
+		})
+		if n > 0 && len(dir) == n {
+			break
+		}
+	}
+	if err := e.Err(); err != nil {
+		return nil, err
+	}
+	if n > 0 && len(dir) < n {
+		return dir, io.EOF
+	}
+	return dir, nil
+}
+
+type dirEntry struct{ fileNode }
+
+func (d dirEntry) Name() string               { return pathpkg.Base(string(d.p)) }
+func (d dirEntry) IsDir() bool                { return d.m.IsDir() }
+func (d dirEntry) Type() fs.FileMode          { return d.m }
+func (d dirEntry) Info() (fs.FileInfo, error) { return d.Stat() }
+
+type fileNode struct {
+	files.Node
+	p path.Path
+	m fs.FileMode
+}
+
+func (n fileNode) Stat() (fs.FileInfo, error) {
+	size, err := n.Size()
+	if err != nil {
+		return nil, err
+	}
+	return fileInfo{
+		p:    n.p,
+		size: size,
+		m:    n.m,
+	}, nil
+}
+
+type fileInfo struct {
+	p    path.Path
+	size int64
+	m    fs.FileMode
+}
+
+func (f fileInfo) Name() string       { return pathpkg.Base(string(f.p)) }
+func (f fileInfo) Size() int64        { return f.size }
+func (f fileInfo) Mode() fs.FileMode  { return f.m }
+func (f fileInfo) ModTime() time.Time { return time.Time{} }
+func (f fileInfo) IsDir() bool        { return f.m.IsDir() }
+func (f fileInfo) Sys() interface{}   { return nil }
--- ipfs_test.go
+++ ipfs_test.go
@@ -2,8 +2,8 @@ package ipfs_test
 
 import (
 	"context"
+	"io"
 	"testing"
-	"testing/fstest"
 
 	"github.com/hyde042/ipfs"
 )
@@ -13,7 +13,20 @@ func TestIPFS(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if err := fstest.TestFS(fsys, "readme"); err != nil {
+	/*
+		if err := fstest.TestFS(fsys, "readme"); err != nil {
+			t.Fatal(err)
+		}
+	*/
+	f, err := fsys.Open("ipfs/QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG/readme")
+	if err != nil {
 		t.Fatal(err)
 	}
+	defer f.Close()
+
+	data, err := io.ReadAll(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(string(data)) // TODO: do some actual validation
 }
